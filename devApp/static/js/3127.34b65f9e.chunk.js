6a84e74c9569d4825b47e44bffb6538ac13210ae
Alex Ventura <aventura3@huskers.unl.edu>
2024-11-17 18:38:20 -0600

    Update devApp to release 10

---
+++ devApp/static/js/3127.34b65f9e.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 3127.34b65f9e.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkhusker_scope=self.webpackChunkhusker_scope||[]).push([[3127],{3127:function(e,r,u){u.r(r),u.d(r,{i:function(){return n}});var n=function(e){return e&&""!==e.dir?"rtl"===e.dir.toLowerCase():"rtl"===(null===document||void 0===document?void 0:document.dir.toLowerCase())}}}]);
+//# sourceMappingURL=3127.34b65f9e.chunk.js.map
